7db25039ac1e09e313f44007b414e9db1803de05
09x3086 <09x3086@gmail.com>
2020-02-10 10:01:07 +0900

    impl

---
+++ earth-impl/src/main/java/model/Hydrogen.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Hydrogen class.
+ *
+ * @author 09x3086
+ */
+@AllArgsConstructor
+@Getter
+public class Hydrogen {
+
+}
